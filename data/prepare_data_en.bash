a77dfc8f7f43253580f476f799deca0346ca6314
Jacob Buckman <buckman@buckman-macbookpro.roam.corp.google.com>
2017-11-27 10:00:47 -0800

    fixed data load script

--- data/prepare_data_en.bash
+++ data/prepare_data_en.bash
@@ -1,8 +1,11 @@
 python get_data.py --language english --download
 
-mkdir en/bpe
-mkdir en/bpe/train
-mkdir en/bpe/heldout
+mv en/raw/1-billion-word-language-modeling-benchmark-r13output/training-monolingual.tokenized.shuffled en/raw/train
+mv en/raw/1-billion-word-language-modeling-benchmark-r13output/heldout-monolingual.tokenized.shuffled en/raw/heldout
+
+mkdir -p en/bpe
+mkdir -p en/bpe/train
+mkdir -p en/bpe/heldout
 
 for i in $(seq -f "%02g" 1 99)
 do
@@ -14,8 +17,8 @@ do
   python apply_bpe.py -c en/codes.txt --vocabulary en/bpe_vocab.txt --vocabulary-threshold 50 < en/raw/heldout/news.en.heldout-000$i-of-00050 > en/bpe/heldout/news.en.heldout-000$i-of-00050 &
 done
 
-mkdir en/bpe/valid
-mkdir en/bpe/test
+mkdir -p en/bpe/valid
+mkdir -p en/bpe/test
 
 cp en/bpe/heldout/* en/bpe/test
 mv en/bpe/test/news.en.heldout-00001-of-00050 en/bpe/valid
