3f54ab3870f5da11fa746cfd2eda0c9c92e4e19a
Jacob Buckman <buckman@buckman-macbookpro.roam.corp.google.com>
2017-11-26 19:06:39 -0800

    added code for BPE (taken from Sennrich et al) and for installing DyNet (taken from documentation)

---
+++ dynet_setup.bash
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Code to easily set up DyNet on a new GCE instance w/ CUDA. Mostly replicated from DyNet tutorial.
+
+apt-get update
+apt-get install python-pip build-essential cmake mercurial
+
+pip install --upgrade pip
+pip install cython
+
+mkdir dynet-base
+cd dynet-base
+
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/$CUDA_REPO_PKG
+dpkg -i $CUDA_REPO_PKG
+apt-get -y update
+apt-get install -y --no-install-recommends cuda-drivers cuda-core-$CUDA_PKG_VERSION cuda-cudart-dev-$CUDA_PKG_VERSION cuda-cublas-dev-$CUDA_PKG_VERSION cuda-curand-dev-$CUDA_PKG_VERSION
+ln -s /usr/local/cuda-${CUDA_VERSION_MAJOR}.${CUDA_VERSION_MINOR} /usr/local/cuda
+
+git clone https://github.com/clab/dynet.git
+hg clone https://bitbucket.org/eigen/eigen -r 699b659
+cd dynet
+mkdir build
+cd build
+
+cmake .. -DEIGEN3_INCLUDE_DIR=../../eigen -DPYTHON=`which python` -DBACKEND=cuda
+
+make -j 16
+cd python
+python ../../setup.py build --build-dir=.. --skip-build install
